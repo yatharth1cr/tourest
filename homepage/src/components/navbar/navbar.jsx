f77bee225254499f029984b157af9fe84d0320c0
yatharth1cr <yatharthgiri187@gmail.com>
2024-12-14 15:14:14 +0530

    completed with navbar

--- homepage/src/components/navbar/navbar.jsx
+++ homepage/src/components/navbar/navbar.jsx
@@ -1,1 +1,42 @@
- 
+import "./navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Navbar() {
+  return (
+    <header className="header">
+      <div className="container d-flex justify-content-between align-items-center">
+        {/* Logo */}
+        <h1 className="logo">Tourest</h1>
+
+        {/* Navigation */}
+        <nav className="navbar">
+          <ul className="navbar-list d-flex flex-column flex-lg-row">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Tours</a>
+            </li>
+            <li>
+              <a href="#">Destinations</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Booking Button */}
+        <button className="btn-primary">Booking Now</button>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
